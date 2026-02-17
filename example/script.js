eb8abe0cd235263c9e2e616d3f9a76548902bfac
Matt Blaul <mattblaul@gmail.com>
2026-02-16 22:30:28 -0500

    Adding color swatches

--- example/script.js
+++ example/script.js
@@ -1,3 +1,7 @@
+window.onload = () => {
+  createColorGrid();
+};
+
 document.getElementById("theme-toggle").addEventListener("click", toggleTheme);
 
 function toggleTheme(event) {
@@ -22,4 +26,39 @@ function toggleTheme(event) {
       event.target.innerHTML = "Switch to light mode";
     }
   }
+}
+
+function createColorGrid() {
+  const colorGrid = document.getElementById("color-grid");
+
+  const colors = ["blue", "pink", "gold", "beige", "grey"];
+
+  const swatchValues = colors.map((color) => {
+    let result = [];
+
+    for (let i = 0; i <= 9; i++) {
+      if (i === 0) {
+        result.push(`${color}-50`);
+      } else {
+        result.push(`${color}-${i * 100}`);
+      }
+    }
+
+    return result;
+  });
+
+  swatchValues.forEach((swatchColumn) => {
+    const colorColumn = document.createElement("div");
+    colorColumn.classList.add("color-column");
+
+    swatchColumn.forEach((swatchValue) => {
+      const colorBox = document.createElement("div");
+      colorBox.classList.add("color-swatch");
+
+      colorBox.style.backgroundColor = `var(--${swatchValue})`;
+      colorColumn.appendChild(colorBox);
+    });
+
+    colorGrid.appendChild(colorColumn);
+  });
 }
